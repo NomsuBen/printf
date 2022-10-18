a2749fd1ca1213059dc1ce16facd8d7aec1d7555
NomsuBen <olaniranbenjamin@gmail.com>
2022-10-18 15:47:51 -0700

    printf

--- get_width.c
+++ get_width.c
@@ -4,7 +4,8 @@
  * get_width - Calculates the width for printing
  * @format: Formatted string in which to print the arguments.
  * @i: List of arguments to be printed.
- * @list: list of arguments
+ * @list: list of arguments.
+ *
  * Return: width.
  */
 int get_width(const char *format, int *i, va_list list)
